a812b8d18487d52b067c48e16037e470fe75ec56
iykeanthony <iykeu73@gmail.com>
2020-08-21 05:44:59 -0700

    first complete

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('.menu-btn')
+
+btn.addEventListener('click', ()=>{
+	document.querySelector('.main-menu').classList.toggle('show')
+})
+
+
+console.log('hello world')
